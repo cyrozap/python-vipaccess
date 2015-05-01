cyrozap/python-vipaccess

commit 65fa72b9621024e3b0074d72b61cfbe29ba12d83

Fixed urllib error

vipaccess/utils.py
@@ -24,7 +24,11 @@ import hmac
 import string
 import sys
 import time
-import urllib
+# Python 2/3 compatibility
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 import qrcode
 import requests
@@ -213,10 +217,10 @@ def decrypt_key(token_iv, token_cipher):
 def generate_otp_uri(token_id, secret):
     '''Generate the OTP URI.'''
     token_parameters = {}
-    token_parameters['otp_type'] = urllib.quote('totp')
-    token_parameters['app_name'] = urllib.quote('VIP Access')
-    token_parameters['account_name'] = urllib.quote(token_id)
-    token_parameters['parameters'] = urllib.urlencode(
+    token_parameters['otp_type'] = urlparse.quote('totp')
+    token_parameters['app_name'] = urlparse.quote('VIP Access')
+    token_parameters['account_name'] = urlparse.quote(token_id)
+    token_parameters['parameters'] = urlparse.urlencode(
         dict(
             secret=base64.b32encode(secret).upper(),
             issuer='Symantec'

commit 8a68c1459567ad1be68196e8f72ca7b1bd8f7a85

Fixed all builds after regressions in 65fa72b and 6975116

vipaccess/utils.py
@@ -26,9 +26,9 @@ import sys
 import time
 # Python 2/3 compatibility
 try:
-    from urllib.parse import urlparse
+    import urllib.parse as urllib
 except ImportError:
-    import urllib as urlparse
+    import urllib
 
 import qrcode
 import requests